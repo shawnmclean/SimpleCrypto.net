f19d0162ba8ea4f3a1824804e88587633c07600f
Shawn Mclean <mixmasterxp@gmail.com>
2012-04-08 00:28:32 -0500

    Added an interface and shell implementation of pbkdf2 crypto

---
+++ src/Interfaces/IPasswordCrypto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrypto
+{
+    public interface ICryptoService
+    {
+        int HashIterations { get; set; }
+
+        int SaltSize { get; set; }
+
+        string PlainText { get; set; }
+
+        string HashedText { get; }
+
+        string Salt { get; }
+
+        string Compute(string textToHash);
+
+        string Compute(string textToHash, int saltSize);
+
+        string Compute(string textToHash, int saltSize, int hashIterations);
+    }
+}
---
+++ src/PBKDF2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrypto
+{
+    public class PBKDF2 : ICryptoService
+    {
+        public PBKDF2()
+        {
+            //Set defaults
+            HashIterations = 50;
+            SaltSize = 16;
+        }
+
+        public int HashIterations
+        { get; set; }
+
+        public int SaltSize
+        { get; set; }
+
+        public string PlainText
+        { get; set; }
+
+        public string HashedText 
+        { get; private set; }
+
+        public string Salt 
+        { get; private set; }
+
+        public string Compute(string textToHash)
+        {
+            PlainText = textToHash;
+
+
+
+            return HashedText;
+        }
+
+        public string Compute(string textToHash, int saltSize)
+        {
+            SaltSize = saltSize;
+            Compute(textToHash);
+            return HashedText;
+        }
+
+        public string Compute(string textToHash, int saltSize, int hashIterations)
+        {
+            HashIterations = hashIterations;
+            Compute(textToHash, saltSize);
+            return HashedText;
+        }
+    }
+}
