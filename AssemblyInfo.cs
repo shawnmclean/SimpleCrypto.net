183714d9fff6745168500bcaf5cca4f3a548fc12
Shawn Mclean <mixmasterxp@gmail.com>
2012-09-16 14:49:44 -0500

    minor version bump for new feature GeneratePassword Feature.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("SimpleCrypto")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.1.0.2")]
-[assembly: AssemblyFileVersion("0.1.0.2")]
-
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
