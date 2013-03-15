039b0db71cf9bb4d45578c8e684d45bc92c6c2fa
Shawn Mclean <mixmasterxp@gmail.com>
2013-03-14 23:00:45 -0500

    Version Bumped to 0.3.0.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("SimpleCrypto")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
