a75fba1dc9310b91683eb1deb4d743901bfc9fb7
Shawn Mclean <mixmasterxp@gmail.com>
2013-03-25 13:03:01 -0500

    added nuspec file.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyCompany("Self")]
-[assembly: AssemblyProduct("SimpleCrypto")]
-[assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
+[assembly: AssemblyCompany("Xormis")]
+[assembly: AssemblyProduct("SimpleCrypto.Net")]
+[assembly: AssemblyCopyright("Shawn Mclean (c) 2013")]
 [assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
